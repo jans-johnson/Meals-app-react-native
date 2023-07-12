d3c805e7ce4b9f22d21f5110b45ee7abb45e5b41
Jans Johnson <jaansjohnson@gmail.com>
2023-07-12 09:35:57 +0530

    Added drawer and favourites icon

--- App.tsx
+++ App.tsx
@@ -1,17 +1,29 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import MealsOverview from './screens/MealsOverview';
+import FavouritesScreen from './screens/FavouritesScreen';
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
+  function Home():JSX.Element{
+    return(
+      <Drawer.Navigator>
+        <Drawer.Screen name='Categories' component={CategoriesScreen}/>
+        <Drawer.Screen name='favourites' component={FavouritesScreen}/>
+      </Drawer.Navigator>
+    )
+  }
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+        <Stack.Screen name='Home' component={Home} options={{headerShown:false}}/>
         <Stack.Screen name='MealsOverview' component={MealsOverview}/>
       </Stack.Navigator>
     </NavigationContainer>
--- screens/MealsOverview.tsx
+++ screens/MealsOverview.tsx
@@ -1,8 +1,9 @@
-import {StyleSheet, FlatList, View} from 'react-native';
+import {StyleSheet, FlatList, View, Button, Pressable} from 'react-native';
 import React,{useLayoutEffect} from 'react';
 import {CATEGORIES, MEALS} from '../data/dummy-data';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import MealItem from '../components/MealItem';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const MealsOverview = () => {
   const route = useRoute();
@@ -19,6 +20,13 @@ const MealsOverview = () => {
     )?.title
     navigation.setOptions({
         title:categoryTitle,
+        headerRight: () => (
+          <Pressable onPress={()=>{
+            console.log('pressed')
+          }}>
+            <Icon name="star" size={30} color="#950" />
+          </Pressable>
+        ),
     })
   },[])
   return (

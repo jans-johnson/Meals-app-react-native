b2522ad15df22cb90d1d808e0ebde8f1f5f878b6
Jans Johnson <jaansjohnson@gmail.com>
2023-07-12 08:24:37 +0530

    useLayout Update

--- screens/MealsOverview.tsx
+++ screens/MealsOverview.tsx
@@ -1,16 +1,26 @@
 import {StyleSheet, FlatList, View} from 'react-native';
-import React from 'react';
-import {MEALS} from '../data/dummy-data';
-import {useRoute} from '@react-navigation/native';
+import React,{useLayoutEffect} from 'react';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import MealItem from '../components/MealItem';
 
 const MealsOverview = () => {
   const route = useRoute();
+  const navigation=useNavigation()
   const {itemId} = route.params as {itemId: string};
 
   const displayedMeals = MEALS.filter(mealItem => {
     return mealItem.categoryIds.indexOf(itemId) >= 0;
   });
+
+  useLayoutEffect(()=>{
+    const categoryTitle = CATEGORIES.find(
+        (category) => category.id===itemId
+    )?.title
+    navigation.setOptions({
+        title:categoryTitle,
+    })
+  },[])
   return (
     <View>
       <FlatList
